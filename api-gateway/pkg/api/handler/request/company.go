29f9649ec76aaa11d2567a7324ab629faf14e6bc
nikhilnarayanan623 <nikhilnarayanan623@gmail.com>
2023-10-30 23:52:13 +0530

    api gateway respons structure updated

--- api-gateway/pkg/api/handler/request/company.go
+++ api-gateway/pkg/api/handler/request/company.go
@@ -3,5 +3,5 @@ package request
 type CompanyRequest struct {
 	Name           string `json:"name" binding:"required,min=3,max=15"`
 	CEO            string `json:"ceo" binding:"required,min=3,max=25"`
-	TotalEmployees int    `json:"total_employees" binding:"required,min=30"`
+	TotalEmployees int    `json:"total_employees" binding:"required,min=100,max=10000"`
 }
